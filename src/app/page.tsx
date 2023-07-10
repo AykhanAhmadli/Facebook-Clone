653a306fc428cf389c02a803fbcf58195108f0e9
aykhanitto <botl12020@gmail.com>
2023-07-10 16:18:54 +0400

    Logo and Search bar were added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Header from '@/components/Header'
 export default function Home() {
   return (
     <main className="">
-        <div>Welcome to my facebook  </div>
+        <div> </div>
         <Header/>
     </main>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
+import facebook from '../assets/facebook1.png'
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <div>Header</div>
+    <div className="p-4 flex items-center justify-between border-b lg:px-10">
+        {/* left  */}
+        <div className="flex items-center mr-2">
+            <div className="w-10 h-10">
+                <Image src={facebook} alt={'facebook logo'} />
+            </div>
+            <div className="ml-2 ">
+          <input
+            type="text"
+            placeholder="Search Facebook"
+            className="outline-0 bg-[#f2f3f7] p-2 rounded-full pl-4 hidden sm:block"
+          />
+        </div>
+        </div>
+    </div>
   )
 }
 
