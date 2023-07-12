234d9ba6023cc3cace1474dfc76ec3805f6fae6b
aykhanahmadli <botl12020@gmail.com>
2023-07-12 17:32:17 +0400

    leftsidebar style changes

--- src/components/Feed/LeftSideBar.tsx
+++ src/components/Feed/LeftSideBar.tsx
@@ -20,51 +20,57 @@ type Props = {};
 const LeftSideBar = (props: Props) => {
   return (
     <div className="w-[13rem] hidden sm:block">
-      <div className="flex flex-col pt-4 pl-6 align-center">
+      <div className="flex flex-col pt-4 pl-4 align-center">
         <div className="space-y-6 [&>*]:flex [&>*]:items-center [&>*]:cursor-pointer  ">
-          <div className=" space-x-2">
-            <div className="w-8 h-8">
+          <div className="flex items-center ">
+            <div className="w-10 h-10 flex items-center justify-center">
               <Image
                 src={sheva}
-                className="rounded-full"
+                className="rounded-full w-8 h-8"
                 alt={"profile picture"}
               />
             </div>
-            <span>Aykhan Ahmadli</span>
-          </div>
-          <div className="">
-            <FiPlayCircle className="w-8 h-8 text-face-blue" />
-
-            <p className="ml-2">Watch</p>
+            <p className="ml-2">Aykhan Ahmadli</p>
           </div>
 
           <div className="">
-            <BsPeopleFill className="w-7 h-7 text-face-blue" />
+            <div className="w-10 h-10 flex items-center justify-center">
+              <BsPeopleFill className="w-7 h-7 text-face-blue" />
+            </div>
             <p className="ml-2">Friends</p>
           </div>
-
           <div className="">
-            <MdGroups className="w-8 h-8 text-face-blue" />
+            <div className="w-10 h-10 flex items-center justify-center">
+              <MdGroups className="w-8 h-8 text-face-blue" />
+            </div>
             <p className="ml-2">Groups</p>
           </div>
-
           <div className="">
-            <AiOutlineShop className="w-9 h-9 text-face-blue" />
+            <div className="w-10 h-10 flex items-center justify-center">
+              <AiOutlineShop className="w-9 h-9 text-face-blue" />
+            </div>
             <p className="ml-2">Marketplace</p>
           </div>
-
           <div className="">
-            <FiFlag className="w-7 h-7 text-face-blue" />
-
+            <div className="w-10 h-10 flex items-center justify-center">
+              <FiFlag className="w-7 h-7 text-face-blue" />
+            </div>
             <p className="ml-2">Saved</p>
+          </div>
+          <div className="">
+            <div className="w-10 h-10 flex items-center justify-center">
+              <FiPlayCircle className="w-8 h-8 text-face-blue" />
+            </div>
+            <p className="ml-2">Watch</p>
           </div>
           {/* <div className="flex items-center">
           <AiFillClockCircle className="w-8 h-8" />
           <p className="ml-2 font-bold">Memories</p>
         </div> */}
-
           <div className="">
-            <RiArrowDownSLine className="w-8 h-8" />
+            <div className="w-10 h-10 flex items-center justify-center">
+              <RiArrowDownSLine className="w-8 h-8" />
+            </div>
             <p className="ml-2 ">See More</p>
           </div>
         </div>
