af88b42a6f622706780b7a647f54e9ace6521b8b
aykhanahmadli <botl12020@gmail.com>
2023-07-12 14:11:23 +0400

    Profile picture added to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import Image from 'next/image'
-import facebook from '../assets/facebook1.png'
+import React from "react";
+import Image from "next/image";
+import facebook from "../assets/facebook1.png";
 import { MdHome } from "react-icons/md";
 import { FiPlayCircle, FiFlag, FiMessageCircle } from "react-icons/fi";
 import { BsCart3 } from "react-icons/bs";
 import { GrGroup, GrAppsRounded } from "react-icons/gr";
 import { FaBell } from "react-icons/fa";
 import { AiOutlineMessage } from "react-icons/ai";
+import sheva from "../assets/sheva .png";
 
-type Props = {}
+type Props = {};
 
 const Header = (props: Props) => {
   return (
     <div className="p-4 flex items-center justify-between border-b lg:px-10">
-        {/* left  */}
-        <div className="flex items-center mr-2">
-            <div className="w-10 h-10">
-                <Image src={facebook} alt={'facebook logo'} />
-            </div>
-            <div className="ml-2 ">
+      {/* left  */}
+      <div className="flex items-center mr-2">
+        <div className="w-10 h-10">
+          <Image src={facebook} alt={"facebook logo"} />
+        </div>
+        <div className="ml-2 ">
           <input
             type="text"
             placeholder="Search Facebook"
             className="outline-0 bg-[#f2f3f7] p-2 rounded-full pl-4 hidden sm:block"
           />
         </div>
-        </div>
-        {/* middle */}
-        <div className="flex items-center space-x-7">
+      </div>
+      {/* middle */}
+      <div className="flex items-center space-x-7">
         <MdHome className="w-9 h-9" />
         <FiFlag className="w-7 h-7" />
         <FiPlayCircle className="w-7 h-7" />
@@ -36,14 +37,17 @@ const Header = (props: Props) => {
       </div>
       {/* right */}
       <div>
-      <div className="md:flex space-x-6 hidden ">
+        <div className="md:flex space-x-6 hidden items-center ">
           <GrAppsRounded className="w-7 h-7" />
           <FaBell className="w-7      h-7" />
           <AiOutlineMessage className="w-7 h-7" />
+          <div className="w-10 h-10">
+            <Image src={sheva} className="rounded-full" alt={""} />
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
